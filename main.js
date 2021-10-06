1db30d34fb0ad3058801dea8dcd9de25db533bc6
Truong Quy <nguyentruongquy75@gmail.com>
2021-10-06 18:50:46 +0700

    Fix port

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+const socket = io("http://localhost:" + PORT);
 const chatInput = document.querySelector(".chat-input");
 const chatBtn = document.querySelector(".chat-btn");
 const messages = document.querySelector(".messages");
