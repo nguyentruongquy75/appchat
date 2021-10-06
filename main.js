32f9dac26c0f7d1fb79cf0bed2f29bc35eeccd4c
Truong Quy <nguyentruongquy75@gmail.com>
2021-10-06 21:33:06 +0700

    edit port

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const socket = io("http://localhost:" + PORT);
 const chatInput = document.querySelector(".chat-input");
 const chatBtn = document.querySelector(".chat-btn");
